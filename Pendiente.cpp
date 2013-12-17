7c3ea1e31291993679119350a18d797cc75e522a
Pedro José Piquero Plaza <gowikel@gmail.com>
2013-12-17 11:21:20 +0100

    Implementado el constructor vacio de Pendiente

---
+++ Pendiente.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Pendiente.hpp"
+
+using namespace std;
+
+namespace algoritmia
+{
+	Pendiente::Pendiente()
+	{
+		Punto x0;
+		Punto x1;
+
+		x0.setPunto(0, 0);
+		x1.setPunto(1, 0);
+
+		this->establecerPendiente(x0, x1);
+	}
+}
