2ed70393b1e49968dd4e6e5c596484f574d9c1cf
Pedro José Piquero Plaza <gowikel@gmail.com>
2013-12-17 11:27:12 +0100

    Implementado el metodo establecerPendiente

--- Pendiente.cpp
+++ Pendiente.cpp
@@ -25,4 +25,28 @@ namespace algoritmia
 	{
 		*this = p;
 	}
+
+	void Pendiente::establecerPendiente(const Punto &A, const Punto &B)
+	{
+		// La pendiente sera Iy / Ix siendo:
+		// Iy --> El incremento de Y
+		// Ix --> El incremento de X
+		double Ix;
+		double Iy;
+
+		Ix = B.getX() - A.getX();
+		Iy = B.getY() - A.getY();
+
+		// Si Ix == 0 entonces la pendiente es infinita
+		if(Ix == 0)
+		{
+			_typeValue = INFINITA;
+			_value = 0;
+		}
+		else
+		{
+			_typeValue = NUMERICA;
+			_value = Iy / Ix;
+		}
+	}
 }
